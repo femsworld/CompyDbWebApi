dadf3ccef29daafd234661b67a2cc47f31e513e2
femsworld <femsworld@gmail.com>
2023-07-26 00:59:43 +0300

    Added ApplicationDBContext

--- Courseprject.Common/Model/Job.cs
+++ Courseprject.Common/Model/Job.cs
@@ -2,5 +2,6 @@
 
 public class Job : BaseEntity
 {
-
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
 }
---
+++ Courseprject.Common/Model/Team.cs
@@ -0,0 +1,8 @@
+﻿namespace Courseprject.Common.Model;
+
+public class Team : BaseEntity
+{
+    string Name { get; set; } = default!;
+    public List<Employee> Employees { get; set; } = default!;
+
+}
